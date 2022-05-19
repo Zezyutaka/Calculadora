b5c1e1d1b34e38886b9985c0007826924902f7c4
Zezyutaka <103161481+Zezyutaka@users.noreply.github.com>
2022-05-19 09:39:52 -0300

    JS concluido

--- cal.js
+++ cal.js
@@ -1,5 +1,20 @@
-function clicar(){
-  let numero = document.querySelector('div#numero')
-  let char = document.querySelector('div#char')
-  let clear = document.querySelector('div#char2')
+  // let numero = document.querySelector('div#numero'.value)
+  // let char = document.querySelector('div#char'.value)
+  // let clear = document.querySelector('div#char2'.value)
+  // let resultado = document.querySelector('div#resultado'.value)
+
+function clicar(num) {
+  let numero = document.querySelector('div#resultado').innerHTML;
+  document.querySelector('div#resultado').innerHTML = (numero + num).substring(0,12);
+}
+
+function limpar() {
+  document.querySelector('div#resultado').innerHTML = "";
+}
+
+function calcular() {
+  var resultado = document.querySelector('div#resultado').innerHTML;
+  if(resultado){
+    document.querySelector('div#resultado').innerHTML = eval(resultado);
+  }
 }
