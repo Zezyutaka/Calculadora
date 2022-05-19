e0d8103677edd2d5601a56c4f85d84573b350ae7
Zezyutaka <moises.scarpin@gmail.com>
2022-05-19 11:23:16 -0300

    feat: adiciona event listener dinamicamente

--- cal.js
+++ cal.js
@@ -1,7 +1,26 @@
+const inputs = document.querySelectorAll('input')
+for (const input of inputs) {
+  input.addEventListener('click', clicar)
+}
 
-function clicar(num) {
-  let numero = document.querySelector('div#resultado').innerHTML;
-  document.querySelector('div#resultado').innerHTML = (numero + num).substring(0,12);
+function clicar(event) {
+  const button = event.target
+  const num = button.value
+  console.log(num)
+  switch (num) {
+    case ',':
+      n = '.'
+      break
+    case 'C':
+      return limpar()
+    case '2':
+      break
+    default:
+      n = parseInt(num)
+  }
+
+  // let numero = document.querySelector('div#resultado').innerHTML;
+  // document.querySelector('div#resultado').innerHTML = (numero + num).substring(0,12);
 }
 
 function limpar() {
@@ -10,7 +29,8 @@ function limpar() {
 
 function calcular() {
   var resultado = document.querySelector('div#resultado').innerHTML;
-  if(resultado){
+  if (resultado) {
     document.querySelector('div#resultado').innerHTML = eval(resultado);
   }
 }
+
